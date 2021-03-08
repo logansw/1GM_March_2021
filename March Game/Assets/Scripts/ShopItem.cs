baa1ea14876f5f5639d3a5954a1d394f74b78472
graywang42 <graywang42@gmail.com>
2021-03-07 23:40:52 -0800

    Created ShopItem gameobject

---
+++ March Game/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    public GameObject itemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(itemPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
