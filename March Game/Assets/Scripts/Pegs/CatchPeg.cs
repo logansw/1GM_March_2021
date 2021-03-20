7a46e08b3de0667b327dd33969f1ae93451434ab
graywang42 <graywang42@gmail.com>
2021-03-20 13:21:33 -0700

    Worked on Catch Peg

--- March Game/Assets/Scripts/Pegs/CatchPeg.cs
+++ March Game/Assets/Scripts/Pegs/CatchPeg.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CatchPeg : Peg
 {
+    // ** Collider of interest is *small grey circle*
+    // Use OnCollisionStay2D
+    // Every frame, gets information about overlapping colliders
+    // Check velocity of collision object
+    // If velocity is less than a threshold, capture ball
+
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
-        // Emit shockwave... spawn shockwave?
-        // Shockwave deals damage
     }
 }
