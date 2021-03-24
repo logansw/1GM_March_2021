aff23562d5c91c0750acaaf9ccdefe146ad2e8ea
graywang42 <graywang42@gmail.com>
2021-03-23 22:07:21 -0700

    Tweaked Tower Values

--- March Game/Assets/Scripts/Peg Scripts/SpikePeg.cs
+++ March Game/Assets/Scripts/Peg Scripts/SpikePeg.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpikePeg : Peg
 {
-    public int damage = 1;
+    public int damage;
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
