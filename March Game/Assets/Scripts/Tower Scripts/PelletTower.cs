f5f1134c109d4240e8b918bbc7479420cdb7326f
graywang42 <graywang42@gmail.com>
2021-03-25 03:40:36 -0700

    Tweaked a bunch of towers

--- March Game/Assets/Scripts/Tower Scripts/PelletTower.cs
+++ March Game/Assets/Scripts/Tower Scripts/PelletTower.cs
@@ -46,7 +46,10 @@ public class PelletTower : Entity
             if (target != null)
             {
                 // AimTowards(target);
-                LeadTarget(target);
+                // LeadTarget(target);
+                // LeadTarget2(target);
+                // NewtonShot(target);
+                NewtonShot2(target);
             }
             // Update timer
             reloadTimer -= Time.deltaTime;
@@ -116,6 +119,96 @@ public class PelletTower : Entity
         Vector3 direction = pos2 - transform.position;
         direction.z = 0;
         transform.up = direction;
+    }
+
+    // Doesn't Work
+    protected void NewtonShot(GameObject target)
+    {
+        Vector3 velocity = target.GetComponent<Rigidbody2D>().velocity;
+
+        // t0 = 0
+        // pos 0 known from target
+        float dist0 = (target.transform.position - transform.position).magnitude;
+
+        float t1 = dist0 / fireSpeed;
+        Vector3 pos1 = target.transform.position + velocity * t1 - 0.5f * t1 * t1 * Physics.gravity;
+        float dist1 = (pos1 - transform.position).magnitude;
+
+        float t2 = dist1 / fireSpeed;
+        Vector3 pos2 = target.transform.position + velocity * t2 - 0.5f * t1 * t1 * Physics.gravity;
+        float dist2 = (pos2 - transform.position).magnitude;
+
+        float t2p = t2 + 0.01f; // Slightly larger t2
+        Vector3 pos2p = target.transform.position + velocity * t2p - 0.5f * t1 * t1 * Physics.gravity; // Slightly dif pos
+        float dist2p = (pos2p - transform.position).magnitude; // Slightly dif dist
+
+        float m = (dist2p - dist2) / (t2p - t2);
+        float b = dist2 - m * t2;
+        float t3 = -b / m;
+        Vector3 pos3 = target.transform.position + velocity * t3;
+        float dist3 = (pos3 - transform.position).magnitude;
+
+        Vector3 direction = pos3 - transform.position;
+        direction.z = 0;
+        transform.up = direction;
+    }
+
+    protected void NewtonShot2(GameObject target)
+    {
+        // Make a function mPos that takes a time input and returns marble position m(t)
+        // Make a function pPos that takes time and direction inputs and returns pellet position p(t)
+            // Direction input is found as m(t) - transform.position
+        // Calculate dist(t) as m(t) - p(t).
+            // This is the function we want to minimize using newton's method.
+        // Newton's Method:
+            // Calculate initial value t0 as dist(0) / fireSpeed.
+            // Make a function slope(t0) that calculates slope:
+                // Calculate slope(t0) = (dist(t0 + dt) - dist(t0)) / (dt). dt = 0.001. Adjust as needed.
+            // Following Newton's method, t1 = t0 - dist(t0) / slope(t0)
+            // Repeat process as tn+1 = tn - dist(tn) / slope(tn).
+
+        // Probably just ignore this stuff for now:
+        // Vector3 v = target.GetComponent<Rigidbody2D>().velocity;
+        Vector3 m = target.transform.position; // Predicted marble position
+        Vector3 p = transform.position; // Predicted pellet position
+        Vector3 v = target.GetComponent<Rigidbody2D>().velocity; // Marble velocity
+        Vector3 u = RadianToVector3(transform.rotation.z) * fireSpeed; // Pellet velocity
+        Debug.Log("m = " + m + " p = " + p + " v = " + v + " u = " + u);
+
+        Vector3 direction = m - transform.position;
+        direction.z = 0;
+        transform.up = direction;
+    }
+
+    private Vector3 newPosition(GameObject target, float t)
+    {
+        Vector3 v = target.GetComponent<Rigidbody2D>().velocity;
+        Vector3 newPosition = target.transform.position + v * t + 0.5f * t * t * Physics.gravity;
+        return newPosition;
+    }
+
+    /*protected void LeadTarget2(GameObject target)
+    {
+        float v = fireSpeed;
+        float x = target.transform.position.x - transform.position.x;
+        float y = target.transform.position.y - transform.position.y;
+        float g = Physics.gravity.y;
+        float theta = Mathf.Atan((v * v - Mathf.Sqrt(Mathf.Pow(v, 4) - g * (g * x * x + 2 * y * v * v))) / (g * x));
+        // Debug.Log("Other things: marble coords " + target.transform.position.x + ", " + target.transform.position.y);
+        // Debug.Log("Tower coords: " + transform.position.x + ", " + transform.position.y);
+        // Debug.Log("LeadTarget2 things: v = " + v + " x = " + x + " y = " + y + " g = " + g + " theta = " + theta);
+        if (x < 0)
+        {
+            theta = Mathf.PI + theta;
+        }
+        Vector3 direction = RadianToVector3(theta);
+        direction.z = 0;
+        transform.up = direction;
+    } */
+
+    public static Vector3 RadianToVector3(float radian)
+    {
+        return new Vector3(Mathf.Cos(radian), Mathf.Sin(radian),0);
     }
 
     public void EnableTower()
