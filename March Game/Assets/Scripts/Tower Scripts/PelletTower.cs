4b9afaf2457a7242c1feef802a15af922a78fe18
graywang42 <graywang42@gmail.com>
2021-03-25 23:48:44 -0700

    Working on Pellet tower obstacle detection, still janky

--- March Game/Assets/Scripts/Tower Scripts/PelletTower.cs
+++ March Game/Assets/Scripts/Tower Scripts/PelletTower.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UIElements;
 
 public class PelletTower : Entity
 {
@@ -43,13 +47,10 @@ public class PelletTower : Entity
         {
             // Point towards nearest target
             target = AcquireTarget();
+            bool noObstacles = false;
             if (target != null)
             {
-                // AimTowards(target);
-                // LeadTarget(target);
-                // LeadTarget2(target);
-                // NewtonShot(target);
-                NewtonShot2(target);
+                AimTowards(target);
             }
             // Update timer
             reloadTimer -= Time.deltaTime;
@@ -58,8 +59,13 @@ public class PelletTower : Entity
             {
                 if (target != null)
                 {
-                    Shoot();
-                    Reload();
+                    noObstacles = NewtonShot(target);
+                    // Shoot some raycasts, if they hit nothing, shoot and reload
+                    if (noObstacles)
+                    {
+                        Shoot();
+                        Reload();
+                    }
                 }
             }
         }
@@ -105,106 +111,84 @@ public class PelletTower : Entity
         transform.up = direction;
     }
 
-    protected void LeadTarget(GameObject target)
+    protected bool NewtonShot(GameObject target)
     {
-        // target
-        Vector3 velocity = target.GetComponent<Rigidbody2D>().velocity;
-        float dist1 = (target.transform.position - transform.position).magnitude;
-        float t1 = dist1 / fireSpeed;
-        Vector3 pos1 = target.transform.position + velocity * t1 - 0.5f*t1*t1*Physics.gravity;
-        float dist2 = (pos1 - transform.position).magnitude;
-        float t2 = dist2 / fireSpeed;
-        Vector3 pos2 = target.transform.position + velocity * t2;
+        Vector3 M0 = target.transform.position; // Initial marble position
+        Vector3 P0 = transform.position; // Initial pellet position
+        Vector3 v0 = target.GetComponent<Rigidbody2D>().velocity; // Marble velocity
 
-        Vector3 direction = pos2 - transform.position;
+        // Calculate initial guess t0
+        float t0 = (M0 - P0).magnitude / fireSpeed;
+        // Calculate next guess t1
+        float d0 = CalcDist(t0, M0, v0, P0);
+        float m = CalcSlope(t0, M0, v0, P0);
+        float t1 = nextNewtonTime(t0, d0, m);
+        // Calculate next guess t2
+        float d1 = CalcDist(t1, M0, v0, P0);
+        float m1 = CalcSlope(t1, M0, v0, P0);
+        float t2 = nextNewtonTime(t1, d1, m1);
+
+        // Aim
+        Vector3 M = M0 + v0 * t2;
+        Vector3 direction = M - P0;
         direction.z = 0;
         transform.up = direction;
-    }
 
-    // Doesn't Work
-    protected void NewtonShot(GameObject target)
-    {
-        Vector3 velocity = target.GetComponent<Rigidbody2D>().velocity;
-
-        // t0 = 0
-        // pos 0 known from target
-        float dist0 = (target.transform.position - transform.position).magnitude;
-
-        float t1 = dist0 / fireSpeed;
-        Vector3 pos1 = target.transform.position + velocity * t1 - 0.5f * t1 * t1 * Physics.gravity;
-        float dist1 = (pos1 - transform.position).magnitude;
-
-        float t2 = dist1 / fireSpeed;
-        Vector3 pos2 = target.transform.position + velocity * t2 - 0.5f * t1 * t1 * Physics.gravity;
-        float dist2 = (pos2 - transform.position).magnitude;
-
-        float t2p = t2 + 0.01f; // Slightly larger t2
-        Vector3 pos2p = target.transform.position + velocity * t2p - 0.5f * t1 * t1 * Physics.gravity; // Slightly dif pos
-        float dist2p = (pos2p - transform.position).magnitude; // Slightly dif dist
-
-        float m = (dist2p - dist2) / (t2p - t2);
-        float b = dist2 - m * t2;
-        float t3 = -b / m;
-        Vector3 pos3 = target.transform.position + velocity * t3;
-        float dist3 = (pos3 - transform.position).magnitude;
-
-        Vector3 direction = pos3 - transform.position;
-        direction.z = 0;
-        transform.up = direction;
-    }
-
-    protected void NewtonShot2(GameObject target)
-    {
-        // Make a function mPos that takes a time input and returns marble position m(t)
-        // Make a function pPos that takes time and direction inputs and returns pellet position p(t)
-            // Direction input is found as m(t) - transform.position
-        // Calculate dist(t) as m(t) - p(t).
-            // This is the function we want to minimize using newton's method.
-        // Newton's Method:
-            // Calculate initial value t0 as dist(0) / fireSpeed.
-            // Make a function slope(t0) that calculates slope:
-                // Calculate slope(t0) = (dist(t0 + dt) - dist(t0)) / (dt). dt = 0.001. Adjust as needed.
-            // Following Newton's method, t1 = t0 - dist(t0) / slope(t0)
-            // Repeat process as tn+1 = tn - dist(tn) / slope(tn).
-
-        // Probably just ignore this stuff for now:
-        // Vector3 v = target.GetComponent<Rigidbody2D>().velocity;
-        Vector3 m = target.transform.position; // Predicted marble position
-        Vector3 p = transform.position; // Predicted pellet position
-        Vector3 v = target.GetComponent<Rigidbody2D>().velocity; // Marble velocity
-        Vector3 u = RadianToVector3(transform.rotation.z) * fireSpeed; // Pellet velocity
-        Debug.Log("m = " + m + " p = " + p + " v = " + v + " u = " + u);
-
-        Vector3 direction = m - transform.position;
-        direction.z = 0;
-        transform.up = direction;
-    }
-
-    private Vector3 newPosition(GameObject target, float t)
-    {
-        Vector3 v = target.GetComponent<Rigidbody2D>().velocity;
-        Vector3 newPosition = target.transform.position + v * t + 0.5f * t * t * Physics.gravity;
-        return newPosition;
-    }
-
-    /*protected void LeadTarget2(GameObject target)
-    {
-        float v = fireSpeed;
-        float x = target.transform.position.x - transform.position.x;
-        float y = target.transform.position.y - transform.position.y;
-        float g = Physics.gravity.y;
-        float theta = Mathf.Atan((v * v - Mathf.Sqrt(Mathf.Pow(v, 4) - g * (g * x * x + 2 * y * v * v))) / (g * x));
-        // Debug.Log("Other things: marble coords " + target.transform.position.x + ", " + target.transform.position.y);
-        // Debug.Log("Tower coords: " + transform.position.x + ", " + transform.position.y);
-        // Debug.Log("LeadTarget2 things: v = " + v + " x = " + x + " y = " + y + " g = " + g + " theta = " + theta);
-        if (x < 0)
+        // Trace pellet trajectory to check for obstacles
+        float tcrit = t2;
+        bool noObstacles = true;
+        float tstep = 0.05f; // Adjust as needed
+        Vector3 Mcrit = M0 + v0 * tcrit; // Get final marble location
+        Vector3 u0 = (Mcrit - P0).normalized * fireSpeed; // Get aiming direction (pellet velocity)
+        Vector3 P1 = P0; // First point is pellet starting point
+        Vector3 P2 = P0 + u0 * tstep + Physics.gravity * tstep * tstep / 2; // Calculate second pellet point after 1 step
+        for (float t = tstep; t < tcrit && noObstacles; t += tstep)
         {
-            theta = Mathf.PI + theta;
+            Debug.Log("Tracing Pellet");
+            // Raycast should ignore marbles
+            P1 = P2;
+            if (t + tstep < tcrit)
+            {
+                P2 = P0 + u0 * (t + tstep) + Physics.gravity * (t + tstep) * (t + tstep) / 2;
+            } else // t + tstep exceeds tcrit, so use tcrit for final value
+            {
+                P2 = P0 + u0 * tcrit + Physics.gravity * tcrit * tcrit / 2;
+            }
+            Debug.DrawRay(P1, P2 - P1, Color.white, 2, false);
+            noObstacles = !Physics.Raycast(P1, P2 - P1, (P2 - P1).magnitude); // Shoot raycast from P1 to P2
         }
-        Vector3 direction = RadianToVector3(theta);
-        direction.z = 0;
-        transform.up = direction;
-    } */
+        Debug.Log("noObstacles? " + noObstacles); // The raycast doesn't seem to detect anything!
+        return noObstacles;
+    }
+
+    #region Newton's Method Functions
+    // Calculates the position of the marble and position of the pellet at time t
+    // Returns the distance bewteen the marble and pellet at time t
+    private float CalcDist(float t, Vector3 M0, Vector3 v0, Vector3 P0)
+    {
+        Vector3 M = M0 + v0 * t; // Marble position
+        Vector3 u0 = (M - P0).normalized * fireSpeed; // Pellet velocity
+        Vector3 P = P0 + u0 * t; // Pellet position
+        float dist = (M - P).magnitude;
+        return dist;
+    }
+
+    private float CalcSlope(float t, Vector3 M0, Vector3 v0, Vector3 P0)
+    {
+        float dt = 0.001f; // Adjust for finer slope
+        float d1 = CalcDist(t, M0, v0, P0);
+        float d2 = CalcDist(t + dt, M0, v0, P0);
+        float m = (d2 - d1) / dt;
+        return m;
+    }
+
+    // Calculates t_n+1 from t_n, f(t_n), and f'(t_n)
+    private float nextNewtonTime(float t0, float d0, float m)
+    {
+        float t1 = t0 - d0 / m;
+        return t1;
+    }
+    #endregion
 
     public static Vector3 RadianToVector3(float radian)
     {
