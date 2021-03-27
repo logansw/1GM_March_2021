e1910de14345c02e07cf686c03270972412e634c
logansw <logansw@uw.edu>
2021-03-26 17:43:12 -0700

    Sell madness

---
+++ March Game/Assets/SellItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellItem : MonoBehaviour
+{
+    private PelletTower pt;
+    [SerializeField] ShopItem shopItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pt = transform.parent.parent.gameObject.GetComponent<PelletTower>();
+        shopItem.cost = -pt.cost;
+    }
+}
