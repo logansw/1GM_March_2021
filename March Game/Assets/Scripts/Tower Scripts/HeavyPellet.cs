9e8ee68411c231578b26c613c3a911aa2b30744f
graywang42 <graywang42@gmail.com>
2021-03-28 01:46:35 -0700

    Heavy pellet tower now has homing missile attack

--- March Game/Assets/Scripts/Tower Scripts/HeavyPellet.cs
+++ March Game/Assets/Scripts/Tower Scripts/HeavyPellet.cs
@@ -6,6 +6,11 @@ public class HeavyPellet : Pellet
 {
     [SerializeField] private float lifetime;
     [SerializeField] private float initialLifetime;
+    [SerializeField] private float homeStrength;
+    // Range
+    [SerializeField] protected float maxRange;
+    // Nearest targetable object
+    protected GameObject target;
 
     protected override void Start()
     {
@@ -21,6 +26,9 @@ public class HeavyPellet : Pellet
         if (lifetime <= 0) {
             Delete();
         }
+        target = AcquireTarget();
+        Vector3 dir = HomingDirection(target);
+        rb.AddForce(dir * homeStrength, ForceMode2D.Impulse);
     }
 
     public override void DealDamage(GameObject other)
@@ -31,5 +39,29 @@ public class HeavyPellet : Pellet
             Marble marble = other.GetComponent<Marble>();
             marble.ChangeHealth(-this.damage);
         }
+    }
+
+    // Finds nearest targetable object
+    protected GameObject AcquireTarget()
+    {
+        GameObject nearestTarget = null;
+        float nearestDist = Mathf.Infinity;
+        foreach (GameObject target in EntityMan.Instance.targetsList)
+        {
+            float distSquared = Mathf.Pow(target.transform.position.x - transform.position.x, 2f)
+                                + Mathf.Pow(target.transform.position.y - transform.position.y, 2f);
+            if (distSquared < nearestDist && distSquared < maxRange)
+            {
+                nearestDist = distSquared;
+                nearestTarget = target;
+            }
+        }
+        return nearestTarget;
+    }
+
+    protected Vector3 HomingDirection(GameObject target)
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        return dir.normalized;
     }
 }
